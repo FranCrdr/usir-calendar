ba283664bfe1eb88c0d47d83970bf964838148c5
[dyad] <git@dyad.sh>
2026-02-12 23:36:49 +0100

    [dyad] Corregido error Edit3 no definido con importación correcta - wrote 1 file(s)

--- src/components/DayCell.tsx
+++ src/components/DayCell.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react';
+import { Edit3 } from 'lucide-react';
 import { ShiftDay, ShiftType, getShiftColor } from '../types/ShiftTypes';
 
 interface DayCellProps {
